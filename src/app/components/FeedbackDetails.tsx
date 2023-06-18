218f810462da3945313d46f4688eec9f0516aa9e
Kamal Soni <iamkamalsoni7@gmail.com>
2023-06-18 22:50:42 +1000

    feedback form

--- src/app/components/FeedbackDetails.tsx
+++ src/app/components/FeedbackDetails.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FeedbackCount from "./FeedbackCount";
+import { BiCaretUp } from "react-icons/bi";
 
 interface feedbackItemProps {
   title: string;
@@ -15,7 +16,10 @@ const FeedbackDetails = ({
   const { title, details, count } = feedbackItem;
   return (
     <div className="flex gap-4 items-center ">
-      <FeedbackCount count={count} />
+      <div className="px-2 py-1 border flex flex-col items-center w-20 h-14 rounded-md">
+        <BiCaretUp />
+        <h2> {count} </h2>
+      </div>
       <div>
         <h1 className="font-bold hover:text-blue-500">{title}</h1>
         <p className="mt-2 text-gray-500">{details}</p>
